c4da19bd1e58f18569beed0d9cc9b93d2fc4b8c4
sammy6310 <kipper98@naver.com>
2022-03-22 19:46:54 +0900

    change comment

--- chExtention.py
+++ chExtention.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     ext = input('변경하려는 확장명을 입력해주세요 (대소문자 구분) : ')
     chExt = input('변경될 확장명을 입력해주세요 (대소문자 구분) : ')
     
-    # 변경 미리 확인
+    # 결과 미리 확인
     changeExtention(path, ext, chExt, True)
     answer = input('다음과 같이 변경됩니다 (y/n) : ')
 
